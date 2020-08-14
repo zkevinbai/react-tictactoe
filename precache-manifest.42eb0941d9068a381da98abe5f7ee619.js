d0b4a01eca559d437a72624d198792e24b693c41
Kevin Bai <lkevin957@gmail.com>
2020-08-13 22:46:20 -0700

    Updates

---
+++ precache-manifest.42eb0941d9068a381da98abe5f7ee619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e98484d91c841ac5915680be33ed96",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "bb53992abc1402309842",
+    "url": "/react-tictactoe/static/css/main.06523173.chunk.css"
+  },
+  {
+    "revision": "adabcee09251987ba023",
+    "url": "/react-tictactoe/static/js/2.fded7595.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tictactoe/static/js/2.fded7595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb53992abc1402309842",
+    "url": "/react-tictactoe/static/js/main.e1919872.chunk.js"
+  },
+  {
+    "revision": "9e7d94a0a69203f46dea",
+    "url": "/react-tictactoe/static/js/runtime-main.813f326f.js"
+  }
+]);
